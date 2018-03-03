3fe11b4e4708cbd48d28135e81f5d3fa9a652920
lenkan <dlenksjo@gmail.com>
2018-03-04 00:07:04 +0100

    Added user store

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import users from './users/user-store'
 
 const app = express()
 
-// TODO: REST API here
+app.get('/users/:id', async (req, res) => {
+  const user = await users.get(req.params.id)
+  res.send(user)
+})
+
+app.put('/users/:id', async (req, res) => {
+  const user = await users.put({ id: req.params.id })
+  res.send(user)
+})
 
 export default app
---
+++ server/users/user-store.js
@@ -0,0 +1,13 @@
+import db from '../db'
+
+const users = db('users')
+
+export default {
+  async get (id) {
+    return users.get(id)
+  },
+
+  async put (user) {
+    return users.put(user)
+  }
+}
