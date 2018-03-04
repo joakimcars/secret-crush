b57708ce52c0f9df9825361667a285fbfe4cd381
lenkan <dlenksjo@gmail.com>
2018-03-04 01:22:07 +0100

    fix config

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 export default {
   server: {
-    port: process.env.SERVER_PORT || 3000,
-    db: {
-      url: process.env.MONGO_SERVER || 'mongodb://localhost:27017',
-      name: process.env.MONGO_DBNAME || 'dev'
-    }
+    port: process.env.SERVER_PORT || 3000
+  },
+  db: {
+    url: process.env.MONGO_SERVER || 'mongodb://localhost:27017',
+    name: process.env.MONGO_DBNAME || 'dev'
   }
 }
