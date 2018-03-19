62ab0fc3b3979c43491862db2e6ff5601e853048
lenkan <dlenksjo@gmail.com>
2018-03-19 23:17:31 +0100

    Fix formatting

--- app/components/app/Header.jsx
+++ app/components/app/Header.jsx
@@ -29,19 +29,18 @@ const Nav = ({ children }) => {
 }
 
 const Header = (props) => {
-
   return (
     <Nav>
       <NavBrand>Secret Crush</NavBrand>
       <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarText' aria-controls='navbarText' aria-expanded='false' aria-label='Toggle navigation'>
         <span className='navbar-toggler-icon' />
-        </button>
-        <div className='collapse navbar-collapse' id='navbarText'>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarText'>
         <Navbar>
           <NavLink to='/'>Home</NavLink>
           {!props.user && <NavLink to='/login'>Login</NavLink>}
-          {props.user &&<NavLink to='/crushes'>Crushes</NavLink>}
-          {props.user &&<NavLink to='/newCrush'>New Crush</NavLink>}
+          {props.user && <NavLink to='/crushes'>Crushes</NavLink>}
+          {props.user && <NavLink to='/newCrush'>New Crush</NavLink>}
           {!props.user && <NavLink to='/register'>Register</NavLink>}
           {props.user && <NavLink to='/account'>Account</NavLink>}
           {props.user && <div onClick={props.logout}><NavLink to='/'>Logout</NavLink></div>}
@@ -51,7 +50,6 @@ const Header = (props) => {
   )
 }
 
-
 function mapStateToProps (state) {
   return {
     user: state.user
@@ -60,9 +58,8 @@ function mapStateToProps (state) {
 
 function mapDispatchToProps (dispatch) {
   return {
-       logout: () => dispatch(logout())
+    logout: () => dispatch(logout())
   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header)
-
