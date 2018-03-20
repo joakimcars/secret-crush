105be8455c17dd3869cd577c2ce3f2b58ab0e854
Joakim Cars <joakim.cars@gmail.com>
2018-03-20 21:30:32 +0100

    styling for new crush

---
+++ app/components/newCrush/NewCrushForm.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+function validate (values) {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Please fill in  the email'
+  }
+  return errors
+}
+
+const FieldInput = ({ meta, label, placeholder, input }) => {
+  const className = [
+    'form-group',
+    meta.touched && meta.error ? 'has-error' : '',
+    meta.touched && !meta.error ? 'has-success' : '',
+    'has-feedback'
+  ].join(' ')
+
+  return (
+    <div className={className}>
+      {label && <label className='control-label' htmlFor={input.name}>{label}</label>}
+      <input {...input} className='form-control' placeholder={placeholder || label} />
+      {meta.touched && !meta.error && <span className='glyphicon glyphicon-ok form-control-feedback' />}
+      {meta.touched && meta.error && <span className='glyphicon glyphicon-remove form-control-feedback' />}
+      {meta.touched && meta.error && <span className='help-block'>{meta.error}</span>}
+    </div>
+  )
+}
+
+const NewCrushForm = props => {
+  const { handleSubmit, valid, submitting } = props
+
+  const containerStyle = {
+    marginLeft: '25%',
+    marginRight: '35%'
+  }
+  const textStyle = {
+    marginTop: '4%',
+    marginLeft: '5%',
+    fontWeight: 'bold'
+  }
+  return (
+    <div style={containerStyle}>
+      <h2> Add a new Crush</h2>
+      <div style={textStyle}>
+        <p>When you add a new crush that person will recieve an email informing them that they have a secret crush. But they will not know that person is you unless they list you as one of their crushes.</p>
+        <form onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <Field label='Crush Email' placeholder='name@domain.com' name='email' type='email' component={FieldInput} />
+            <button type='submit' className='btn btn-primary' disabled={!valid || submitting}>Add Crush</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default reduxForm({ form: 'newcrushform', validate })(NewCrushForm)
--- app/components/newCrush/NewCrushPage.jsx
+++ app/components/newCrush/NewCrushPage.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
-import { users as api } from '../../api'
 import { connect } from 'react-redux'
+import NewCrushForm from './NewCrushForm'
 
-async function newCrush (email, user) {
-  await api.insert(user, email)
+async function registerCrush (email, user) {
+  const newCrush = {
+    email: email,
+    user: user
+  }
+  console.log(newCrush)
 }
 
-class NewCrush extends React.Component {
-  submitForm (e) {
-    e.preventDefault()
-    newCrush(this.email.value, this.props.user)
+const NewCrush = props => {
+  function submitForm (newCrush) {
+    registerCrush(newCrush.email, props.user)
   }
-
-  render () {
-    return (
-      <div>
-        <h3>Add new crush here</h3>
-        <p>Persons you add to your list of crushes will recieve an email informing them that they have a secret crush and encouraged to list their own crushes. They will not know that they are on your crush-list if they dont add you to their list.</p>
-        <form onSubmit={e => this.submitForm(e)}>
-          <label>email</label>
-          <input type='email' ref={node => { this.email = node }} placeholder='name@domain.com' />
-          <button>Add Crush</button>
-        </form>
+  return (
+    <div className='jumbotron'>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <NewCrushForm onSubmit={submitForm} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 function mapStateToProps (state) {
