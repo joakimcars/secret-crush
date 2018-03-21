b332b519b178a920641dfff4034779ec4b2179e8
Joakim Cars <joakim.cars@gmail.com>
2018-03-21 03:12:54 +0100

    enabled account new crush and crushes if no user is logged in

--- app/components/account/AccountPage.jsx
+++ app/components/account/AccountPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { users as api } from '../../api'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 async function removeUser (user) {
   const userToRemove = {
@@ -9,18 +10,39 @@ async function removeUser (user) {
   await api.remove(userToRemove.id)
 }
 
-const AccountPage = ({ user }) => {
-  function submitForm (event) {
-    event.preventDefault()
-    confirm('Are you sure you want to delete this account?')
-    removeUser(user.id)
+const AccountPage = props => {
+  if (props.user.id === undefined) {
+    const containerStyle = {
+      marginLeft: '25%'
+    }
+    const textStyle = {
+      marginTop: '4%',
+      marginLeft: '5%',
+      fontWeight: 'bold'
+    }
+    return (
+      <React.Fragment>
+        <div className='jumbotron'>
+          <div style={containerStyle}>
+            <h2>You are not logged in</h2>
+            <div style={textStyle}>
+              <p><Link to='/login'>Login</Link> to manage your account</p>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
-
+  function submitForm (e) {
+    e.preventDefault()
+    window.confirm('Are you sure you want to delete this account?')
+    removeUser(props.user.id)
+  }
   return (
     <React.Fragment>
       <div>
         <form onSubmit={submitForm}>
-          <h3>{user.id}</h3>
+          <h3>{props.user.id}</h3>
           <p>do you wish to delete your account</p>
           <button>yes</button>
         </form>
--- app/components/newCrush/NewCrushPage.jsx
+++ app/components/newCrush/NewCrushPage.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 import { users as api } from '../../api'
 import { connect } from 'react-redux'
 import NewCrushForm from './NewCrushForm'
+import { Link } from 'react-router-dom'
 
 const NewCrush = props => {
+  if (props.user.id === undefined) {
+    const containerStyle = {
+      marginLeft: '25%'
+    }
+    const textStyle = {
+      marginTop: '4%',
+      marginLeft: '5%',
+      fontWeight: 'bold'
+    }
+    return (
+      <React.Fragment>
+        <div className='jumbotron'>
+          <div style={containerStyle}>
+            <h2>You are not logged in</h2>
+            <div style={textStyle}>
+              <p><Link to='/login'>Login</Link> to add a new crush</p>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
   function submitForm (crush) {
     api.addCrush(props.user.id, crush)
   }
