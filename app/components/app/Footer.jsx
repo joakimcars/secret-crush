50639dbe7f4b173a43620c4a89f6486210948b7c
Joakim Cars <joakim.cars@gmail.com>
2018-03-27 01:41:33 +0200

    made app send email to crush when crush added

--- app/components/app/Footer.jsx
+++ app/components/app/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = (props) => {
   return (
-    <div class='container body-content'>
+    <div className='container body-content'>
       <footer>
         <p>&copy; 2018 Secret Crush - Who is your secret crush?</p>
       </footer>
